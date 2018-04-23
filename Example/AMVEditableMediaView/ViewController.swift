0d71861a1c1deca4d02944b6a4413dd44f8918a3
zorroseph <zorroseph@gmail.com>
2018-04-23 17:56:01 -0300

    Start project

--- Example/AMVEditableMediaView/ViewController.swift
+++ Example/AMVEditableMediaView/ViewController.swift
@@ -7,18 +7,25 @@
 //
 
 import UIKit
+import AMVEditableMediaView
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let persona = Estudiante()
+        print(persona.saludar())
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 }
 
+public class Estudiante : Persona {
+    public override func saludar() {
+        print("Hola Señor")
+    }
+}
---
+++ Example/Pods/Target Support Files/AMVEditableMediaView/AMVEditableMediaView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AMVEditableMediaView : NSObject
+@end
+@implementation PodsDummy_AMVEditableMediaView
+@end
